03d6a69c443d7f65da19377116d7be12114e5ce7
Dan Tuls <spimonid@gmail.com>
2021-06-22 10:07:22 -0400

    show all route complete, implementing WWR import

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,6 +5,19 @@ class JobsController < ApplicationController
 
   def wwr
     response = HTTP.get("https://weworkremotely.com/remote-jobs.rss").body
-    render json: response
+    render json: response.as_json
+
+    # url = "https://weworkremotely.com/remote-jobs.rss"
+    # open(url)
+    # open(url) do |rss|
+    #   feed = RSS::Parser.parse(rss)
+    #   puts "Title: #{feed.channel.title}"
+    #   feed.items.each do |item|
+    #     puts "Item: #{item.title}"
+    #   end
+    # end
+
+    # url = "https://weworkremotely.com/categories/remote-back-end-programming-jobs.rss"
+    # HTTParty.get(url).body
   end
 end
